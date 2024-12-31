69a48fb55a8e1885b41ffd625bca28ce9bd9793a
unknown <shariquataskeen7@gmail.com>
2024-12-31 11:16:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="background">
+      <h1 className="fade-in-text">Hello,G.Shariqua Taskeen!</h1>
+    </div>
+  );
+};
+
+export default App;
